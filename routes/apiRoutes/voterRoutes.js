f8ece4c4d8759244c7c78fd00576aa851ebc02f5
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-17 10:36:43 -0400

    created get routes for all voter data and for individual voter by id

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,3 +3,41 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/database');
+const inputCheck = require('../../utils/inputCheck');
+
+// A route to get all the data from the voters table, sorted by last name
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+    const params = [];
+  
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }  
+      res.json({
+        message: 'success',
+        data: rows
+        });
+    });
+});
+
+// A route to get an individual voter by id
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+module.exports = router;
