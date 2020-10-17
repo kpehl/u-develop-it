9906455f9f4c57e710ca13b9053d85ab09ccfc2c
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-17 10:54:32 -0400

    added routes for a voter updating email address, and voter being deleting from the table; verified that all endpoints are functional in Insomnia Core

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -40,4 +40,66 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// A route to add a new voter
+router.post('/voter', ({ body }, res) => {
+    // check the data to ensure there are no unexpected blank values
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
+    // if all is well, proceed with adding the data, using prepared statements as per best practices
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+  
+    db.run(sql, params, function(err, data) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      } 
+      res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+      });
+    });
+});
+
+// A route for a voter to update their email address
+router.put('/voter/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    // Prepare statement
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+    // Execute
+    db.run(sql, params, function(err, data) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }  
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: this.changes
+      });
+    });
+});
+
+// A route to delete a voter
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`
+    db.run(sql, req.params.id, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({ message: 'deleted', changes: this.changes});
+    });
+});
+
 module.exports = router;
