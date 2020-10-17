d7173407f1c4f979ae10c2ac6301249a90590bae
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-16 22:09:23 -0400

    wrapped SQL routes in Express routes for get all, get single, and delete single candidate; verified via Insomnia Core that routes are working

--- server.js
+++ server.js
@@ -22,31 +22,56 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.')
 });
 
-// // An SQL route to return ALL the data in the candidates table
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     if(err) {
-//         console.log(err);
-//     }    
-//     console.log(rows);
-// });
+// An Express route to return all the data in the SQLite3 candidates table
+// An error will result in a server error (500) sent back to the client
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      } 
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
-// // An SQL route to GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+// An Express route to get a single candidate by id from the SQLite3 candidates table
+// An error will result in a request not accepted (400) sent back to the client
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      } 
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
 
-// // An SQL route to delete a candidate
-// const delSql = `DELETE FROM candidates WHERE id = ?`;
-// const delParams = 1;
-// db.run(delSql, delParams, function(err,result) {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log(result, this, this.changes);
-// });
+// An Express route to delete a candidate
+// An error will result in a request not accepted (400) sent back to the client
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      } 
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+  });
 
 // // An SQL route to create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
